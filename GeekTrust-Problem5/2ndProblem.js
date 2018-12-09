466e10e112cad4afcbaac1168d1f536bf7f24667
mahasagar <sagar.chaudhari@medibox.in>
2018-12-09 17:38:59 +0530

    Geektrust-Problems5-Part2 Comments

--- GeekTrust-Problem5/2ndProblem.js
+++ GeekTrust-Problem5/2ndProblem.js
@@ -1,11 +1,16 @@
+'use strict';
+
+//Message File
 var fs = require('fs');
 var messages = fs.readFileSync('boc-messages.txt', 'utf8');
 messages = messages.split('\n');
 
+//Input
 var stdin = process.openStdin();
 const readline = require('readline');
 var cl = readline.createInterface( process.stdin, process.stdout );
 
+//Kingdom and Its Animal
 var kingdom = {
 	space : 'gorilla',
 	land : 'panda',
@@ -14,10 +19,14 @@ var kingdom = {
 	air : 'owl',
 	fire : 'dragon'
 };
+
+//Final Result Format
 var result = {
 	ruler : null,
 	allies : []
 }
+
+//Print Options to Select!
 function printFirst(){
 	console.log('\n ****** Welcome to Tame Of Thrones ******');
 	console.log('1 : Who is the ruler of Southeros?');
@@ -26,6 +35,7 @@ function printFirst(){
 	console.log('\nCrtl+c : Exit!');
 }
 
+//Check Message contains animal name
 var checkMessage = function(message,animal){
 
    for(var i = 0;i < animal.length; i++){
@@ -40,75 +50,84 @@ var checkMessage = function(message,animal){
    }
    return true;
 };
-function sendMessages(selectedKingdom,allies){
+
+//Send messages to Kingdom!
+function sendMessages(selectedKingdom,competingKingdoms){
     var resultData = {
         count : 0,
         allies : []
     };
     Object.keys(kingdom).forEach(function(oneKingdom) {
-      if(selectedKingdom !== oneKingdom && allies.indexOf(oneKingdom) < 0){
-         var randomMessage = messages[Math.floor(Math.random()*messages.length)];
-         var animal=kingdom[oneKingdom];
-          var status = checkMessage(randomMessage,animal);
-          if(status){
-             resultData.allies.push(oneKingdom);
-             resultData.count++;
-          }
-      }
+		//send message to all kingdom, not to : self and competing kingdom
+      	if(selectedKingdom !== oneKingdom && competingKingdoms.indexOf(oneKingdom) < 0){
+		    //select random message!
+		    var randomMessage = messages[Math.floor(Math.random()*messages.length)];
+         	var animal=kingdom[oneKingdom];
+          	var status = checkMessage(randomMessage,animal);
+          	if(status){
+            	resultData.allies.push(oneKingdom);
+             	resultData.count++;
+          	}
+      	}
     })
     return resultData;
 }
-function competingArrangement(inputMessage){
-	inputMessage = inputMessage.trim();
-	var allies = inputMessage.split(' ');
-	if(allies.length < 2){
-	    console.log('Atleast 2 allies for competing!');
-	    return;
-	}
-	var counting = {};
-	for(var i = 0; i < allies.length; i++){
-	    var oneAllies = allies[i].toLowerCase();
-	    if(!kingdom[oneAllies]){
-	        console.log('Wrong Allies!')
-            return;
-	    }else{
-            var animal = kingdom[oneAllies];
-            var countResult = sendMessages(oneAllies,allies);
-            counting[oneAllies] = countResult;
-	    }
-	}
 
+//select the winner
+function winnerSelection(competingKingdomsResult){
 	var highestCount = 0;
 	var TieAllies =[];
 	var tieStringAgain = '';
 	var winnerAllies = null;
-	Object.keys(counting).forEach(function(countAllies) {
-	     if(highestCount < counting[countAllies].count){
-	        highestCount = counting[countAllies].count;
-	        winnerAllies = countAllies;
-	        TieAllies =[];
-	        tieStringAgain = countAllies;
-	        TieAllies.push(countAllies);
-	     }else if(highestCount != 0 && highestCount == counting[countAllies].count){
-	        TieAllies.push(countAllies);
-	        tieStringAgain = tieStringAgain + ' '+countAllies;
+	Object.keys(competingKingdomsResult).forEach(function(oneCompetingKingdom) {
+	     if(highestCount < competingKingdomsResult[oneCompetingKingdom].count){
+	        highestCount = competingKingdomsResult[oneCompetingKingdom].count;
+	        winnerAllies = oneCompetingKingdom;
+	        TieAllies = [oneCompetingKingdom];
+	     }else if(highestCount != 0 && highestCount == competingKingdomsResult[oneCompetingKingdom].count){
+	        TieAllies.push(oneCompetingKingdom);
 	     }
 	});
+	//IF Tie, pass tie competingKingdoms to competingArrangement funcion.
 	if(TieAllies.length > 1){
-    	console.log('\n');
+		tieStringAgain = TieAllies.join(' ');
+    	console.log('Tie in : '+tieStringAgain);
     	competingArrangement(tieStringAgain);
 	}else if(winnerAllies){
 	    result.ruler = winnerAllies;
-	    result.allies = counting[winnerAllies].allies;
+	    result.allies = competingKingdomsResult[winnerAllies].allies;
 	    console.log('*** Done ***');
 	}
 }
+
+//Option 3 
+function competingArrangement(inputMessage){
+	inputMessage = inputMessage.trim();
+	var competingKingdoms = inputMessage.split(' ');
+	//if 0/1 kingdom
+	if(competingKingdoms.length < 2){
+	    console.log('Atleast 2 competingKingdoms for competing!');
+	    return;
+	}
+
+	var competingKingdomsResult = {};
+	//Send Messages to competingKingdoms
+	for(var i = 0;i < competingKingdoms.length;i++){
+	    var oneCompetingKingdom = competingKingdoms[i].toLowerCase();
+	    if(!kingdom[oneCompetingKingdom]){
+	        console.log('Kingdom Not Found!,'+oneCompetingKingdom);
+            return;
+	    }else{
+            var animal = kingdom[oneCompetingKingdom];
+			var countResult = sendMessages(oneCompetingKingdom,competingKingdoms);
+			competingKingdomsResult[oneCompetingKingdom] = countResult;
+	    }
+	}
+
+	winnerSelection(competingKingdomsResult);
+}
 function main(){
     stdin.addListener("data", function(d) {
-	    // note:  d is an object, and when converted to a string it will
-	    // end with a linefeed.  so we (rather crudely) account for that
-	    // with toString() and then trim()
-	    // console.log("you entered: "+d);
 	    switch (parseInt(d)) {
 	    	case 1 :
 	            console.log('\nWho is the ruler of Southeros?');
@@ -123,12 +142,12 @@ function main(){
 	    		 printFirst();
 	    	    break;
 
-	    	case 3 :
-                  cl.question('Enter the kingdoms competing to be the ruler(space seperated): ', (newData) => {
-                  competingArrangement(newData);
-                  printFirst();
+			case 3 :
+					cl.question('Enter the kingdoms competing to be the ruler(space seperated): ', (newData) => {
+					competingArrangement(newData);
+					printFirst();
                 });
-	             break;
+				 break;
 
 	    }
 	  });
